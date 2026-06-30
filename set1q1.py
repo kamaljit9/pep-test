693d31c36b8f61c76ee335c15a7d97921de26c30
Kamaljit kaur <149226505+kamaljit9@users.noreply.github.com>
2026-06-30 11:31:50 +0530

    q1test1
    Refactored code to encapsulate pagination logic in a function.

--- set1q1.py
+++ set1q1.py
@@ -1,12 +1,14 @@
-q1
+#q1
 n=int(input("enter the size:"))
 list1=[]
 for i in range(n):
     x=int(input(""))
     list1.append(x)
-set1=set(list1)
-p=int(input("page size:"))
-list2=list(set1)
-for i in range(0,len(set1),p):
-    x=list2[i:i+p]
-    print(x)
+def create_paginator():
+    set1=set(list1)
+    p=int(input("page size:"))
+    list2=list(set1)
+    for i in range(0,len(set1),p):
+        x=list2[i:i+p]
+        print(x)
+create_paginator()
